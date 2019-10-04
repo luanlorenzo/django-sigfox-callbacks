bb95d9d7cff62f5dcdade37042051f6b970422bf
Luan Lorenzo <luanlorenzo23@gmail.com>
2019-10-04 18:37:37 -0300

    Changes to heroku deploy

--- src/sigfoxproject/settings.py
+++ src/sigfoxproject/settings.py
@@ -64,13 +64,7 @@ WSGI_APPLICATION = 'sigfoxproject.wsgi.application'
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-    }
+    'default': env.db('DATABASE_URL'),
 }
 
 
